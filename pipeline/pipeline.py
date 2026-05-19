fd000d8b1aae0757f531f93876481d5d4db2bc60
Sagar Marthandan <sagar.marthandan@gmail.com>
2026-05-19 12:29:23 +0000

    add more samples to df

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -7,7 +7,12 @@ print("arguments", sys.argv)
 day = int(sys.argv[1])
 print(f"Running pipeline for day {day}")
 
-df = pd.DataFrame({"date": [1, 2], "month": [3, 4], "year": [1992, 1992]})
+
+df = pd.DataFrame({
+    "date": [1, 2, 3, 4, 5],
+    "month": [3, 4, 5, 6, 7],
+    "year": [1992, 1992, 1993, 1994, 1995]
+})
 print(df.head())
 
 df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
